6cfcfb212a72f254f1c7755fdb0932bdc0e266d8
ZainKhalidOfficial <thezainkhalidofficial@gmail.com>
2025-09-06 15:07:45 +0500

    Unit and Challenges - UI & Backend

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,23 +1,39 @@
 import { redirect } from "next/navigation";
 
-import { getUserProgress } from "@/db/queries";
 
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
-import { Header } from "./header";
 import { UserProgress } from "@/components/user-progress";
+
+import { getCourseProgress, getLessonPercentage, getUnits, getUserProgress } from "@/db/queries";
+
+import { Header } from "./header";
+import { Unit } from "./unit";
 
 const LearnPage = async () => {
 
     const userProgressData = getUserProgress();
+    const courseProgressData = getCourseProgress();
+    const lessonPercentageData = getLessonPercentage();
+    const unitsData = getUnits(); 
 
     const [
         userProgress,
+        units,
+        courseProgress,
+        lessonPercentage
     ] = await Promise.all([
-        userProgressData
+        userProgressData,
+        unitsData,
+        courseProgressData,
+        lessonPercentageData
     ]);
 
     if (!userProgress || !userProgress.activeCourse) {
+        redirect("/courses")
+    }
+
+    if(!courseProgress) {
         redirect("/courses")
     }
 
@@ -25,15 +41,30 @@ const LearnPage = async () => {
         <div className="flex flex-row-reverse gap-[48px] px-6">
             <StickyWrapper>
                 <UserProgress
-                    activeCourse = {{title: "English", imageSrc: "/uk.svg"}}
-                    hearts = {5}
-                    points = {100}
+                    activeCourse = {userProgress.activeCourse}
+                    hearts = {userProgress.hearts}
+                    points = {userProgress.points}
                     hasActiveSubscription = {false}
                 />
             </StickyWrapper>
 
             <FeedWrapper>
-                <Header title="English" />
+                <Header title={userProgress.activeCourse.title} />
+                {
+                    units.map((unit) => (
+                        <div key={unit.id} className="mb-10"> 
+                            <Unit
+                                id={unit.id}
+                                order={unit.order}
+                                description={unit.description}
+                                title={unit.title}
+                                lessons={unit.lessons}
+                                activeLesson={courseProgress?.activeLesson}
+                                activeLessonPercentage={lessonPercentage}
+                            />
+                        </div>
+                    ))
+                }
             </FeedWrapper>
         </div>
      );
--- scripts/seed.ts
+++ scripts/seed.ts
@@ -13,6 +13,11 @@ const main = async() => {
         console.log("Seeding Database");
         await db.delete(schema.courses);
         await db.delete(schema.userProgress);
+        await db.delete(schema.units);
+        await db.delete(schema.lessons);
+        await db.delete(schema.challenges);
+        await db.delete(schema.challengeOptions);
+        await db.delete(schema.challengeProgress);
 
         await db.insert(schema.courses).values([
             {
@@ -26,6 +31,85 @@ const main = async() => {
                 imageSrc: "/oet.svg"
             }
         ])
+
+        await db.insert(schema.units).values([
+            {
+                id: 1,
+                courseId: 1, //IELTS (Academic)
+                title: "Unit 1",
+                description: "Learn the basics of IELTS",
+                order: 1
+            }
+        ])
+
+        await db.insert(schema.lessons).values([{
+            id: 1,
+            unitId: 1, //Unit 1 (Learn the basics of IELTS)
+            order: 1,
+            title: "Nouns",
+        },
+        {
+            id: 2,
+            unitId: 1, //Unit 1 (Learn the basics of IELTS)
+            order: 2,
+            title: "Nouns",
+        },
+        {
+            id: 3,
+            unitId: 1, //Unit 1 (Learn the basics of IELTS)
+            order: 3,
+            title: "Nouns",
+        },
+        {
+            id: 4,
+            unitId: 1, //Unit 1 (Learn the basics of IELTS)
+            order: 4,
+            title: "Nouns",
+        },
+        {
+            id: 5,
+            unitId: 1, //Unit 1 (Learn the basics of IELTS)
+            order: 5,
+            title: "Nouns",
+        }
+     ]);
+
+     await db.insert(schema.challenges).values([
+        {
+            id: 1,
+            lessonId: 1, //Nouns
+            type: "SELECT",
+            order: 1,
+            question: 'Which one of these is the "the man"?'
+        },
+     ]);
+
+     await db.insert(schema.challengeOptions).values([
+        {
+            id: 1,
+            challengeId: 1, //'Which one of these is the "the man"?'
+            imageSrc: "/man.svg",
+            correct: true,
+            text: "el hombre",
+            audioSrc: "/es_man.mp3",
+        },
+        {
+            id: 2,
+            challengeId: 1, 
+            imageSrc: "/woman.svg",
+            correct: false,
+            text: "la mujer",
+            audioSrc: "/es_woman.mp3",
+        },
+        {
+            id: 3,
+            challengeId: 1, 
+            imageSrc: "/robot.svg",
+            correct: false,
+            text: "el robot",
+            audioSrc: "/es_robot.mp3",
+        },
+     ])
 
         console.log("Seeding Finished!")
 
